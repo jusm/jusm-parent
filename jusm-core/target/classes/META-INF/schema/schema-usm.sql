4a5c3b789eb462cd43388df0e09f8d66884fd174
wenhaoran <262856298@qq.com>
2018-08-18 17:52:11 +0800

    rr

--- jusm-core/target/classes/META-INF/schema/schema-usm.sql
+++
@@ -1,9 +0,0 @@
---记住我使用的表
-CREATE TABLE IF NOT EXISTS `persistent_logins`(
-	`username` varchar(64) NOT NULL, 
-	`series`   varchar(64) NOT NULL, 
-	`token`    varchar(64) NOT NULL, 
-	`last_used` timestamp  NOT NULL 
-	 DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	 PRIMARY KEY (`series`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
